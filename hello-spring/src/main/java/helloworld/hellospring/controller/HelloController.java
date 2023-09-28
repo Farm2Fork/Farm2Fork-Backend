0e70336e5f828656d15c9d09ad0a812b18491019
Elliott Larsen <elliottlrsn@gmail.com>
2023-09-27 20:59:26 -0600

    API start.

--- hello-spring/src/main/java/helloworld/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/helloworld/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package helloworld.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,29 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("name", "Elliott");
         return "hello"; // Template name.
+    }
+
+    @GetMapping("mvc")
+    public String MVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "mvc";
+    }
+
+    @GetMapping("api")
+    @ResponseBody
+    public User API(@RequestParam("name") String name) {
+        User user = new User();
+        user.setName(name);
+        return user;
+    }
+
+    static class User {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
